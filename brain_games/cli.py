55acd581e62b5ca51287bacb4c2f2df695719910
Radmir <radmirezzz@bk.ru>
2024-06-19 00:50:58 +0500

    upgrade3

---
+++ brain_games/cli.py
@@ -0,0 +1,17 @@
+def welcome_user():
+
+    name = input("May I have your name? ")
+    while name == '':
+        if name == '':
+            name = input("May I have your name? ")
+    else:
+        print("Hello, " + name + "!")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    welcome_user()
---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,14 @@
+#!/usr/bit/env python3
+from brain_games.cli import welcome_user
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    welcome_user()
+
+
+
+
+if __name__ == "__main__":
+    main()
+    welcome_user()
